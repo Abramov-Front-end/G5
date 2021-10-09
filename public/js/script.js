96b1f55fbece0d398835b36c49909e21c5c27a30
Abramov Yevhen <yevhen.abramov@e-consulting.com.ua>
2021-10-09 23:44:54 +0300

    All content block completed

---
+++ public/js/script.js
@@ -0,0 +1,10 @@
+
+const feedbackSwiper = new Swiper('#feedbackSwiper', {
+    slidesPerView: 1,
+    spaceBetween: 20,
+    loop: true,
+    navigation: {
+        nextEl: '.swiper-button.next',
+        prevEl: '.swiper-button.prev',
+    }
+})
